64a11bd80412f2c2441cc41f77117e78169baa53
Varun <tatireddyvarun20@gmail.com>
2022-10-26 11:05:48 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../public/styles.css";
 
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
--- src/pages/Saved/Saved-Nav.jsx
+++ src/pages/Saved/Saved-Nav.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "./Saved-Nav.css";
 function SavedNav() {
