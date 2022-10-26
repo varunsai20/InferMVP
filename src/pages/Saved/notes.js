571c3e8284863f3cae99e03df540dae57f78cbdb
Varun <tatireddyvarun20@gmail.com>
2022-10-26 14:31:15 +0000

    Update notes.js

--- src/pages/Saved/notes.js
+++ src/pages/Saved/notes.js
@@ -10,7 +10,7 @@ const notes = [
   {
     key: 1,
     title: "Fundamentals of typography in user interface design (UI)",
-    tags: "Tags",
+    tags: "Tags :",
     Tags: "UI",
     content:
       "The good news is, there are eight basic, universal typographical design elements: typeface, hierarchy, contrast, consistency, alignment, white, black, purple ...",
@@ -18,7 +18,7 @@ const notes = [
   {
     key: 1,
     title: "Fundamentals of typography in user interface design (UI)",
-    tags: "Tags",
+    tags: "Tags :",
     Tags: "UI",
     content:
       "The good news is, there are eight basic, universal typographical design elements: typeface, hierarchy, contrast, consistency, alignment, white, black, purple ...",
@@ -26,7 +26,7 @@ const notes = [
   {
     key: 1,
     title: "Fundamentals of typography in user interface design (UI)",
-    tags: "Tags",
+    tags: "Tags :",
     Tags: "UI",
     content:
       "The good news is, there are eight basic, universal typographical design elements: typeface, hierarchy, contrast, consistency, alignment, white, black, purple ...",
@@ -34,7 +34,7 @@ const notes = [
   {
     key: 1,
     title: "Fundamentals of typography in user interface design (UI)",
-    tags: "Tags",
+    tags: "Tags :",
     Tags: "UI",
     content:
       "The good news is, there are eight basic, universal typographical design elements: typeface, hierarchy, contrast, consistency, alignment, white, black, purple ...",
