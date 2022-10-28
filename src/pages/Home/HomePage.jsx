3296c610ac7389a53970dfbc271b68b4e3283783
Varun <tatireddyvarun20@gmail.com>
2022-10-28 12:15:01 +0000

    Update 2 files

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import * as FiIcons from "react-icons/fi";
 import Search from "../../components/Searchbar";
 export default function HomePage() {
   return (
     <>
-      <div className="">
-        <img><FiIcons.FiSearch></img>
-      </div>
       <div className="home">
         <h1>Home</h1>
       </div>
