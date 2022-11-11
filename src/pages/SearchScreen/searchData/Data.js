530d860b2be0978ad89fed9a49d5efdb416973ea
Varun <tatireddyvarun20@gmail.com>
2022-11-11 13:26:49 +0000

    Update 3 files

--- src/pages/SearchScreen/searchData/Data.js
+++ src/pages/SearchScreen/searchData/Data.js
@@ -2,12 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Data = ({ data }) => {
   return (
-    <div>
-      <Link to={data.title}>{data.title}</Link>
-      <p>{data.snippet}</p>
+    <div className="SearchPage">
+      <div className="note">
+        <a href="{data.formattedUrl}">{data.title}</a>
+        <div className="TagFrame">
+          <p>Tags : </p>
+          <p className="Tags">{}</p>
+        </div>
+        <p>{data.snippet}</p>
+      </div>
     </div>
   );
 };
 
 export default Data;
 
+//  <div className="saved">
+//       <div className="note">
+//         <a href="">{post.title}</a>
+//         <div className="TagFrame">
+//           <p>Tags : </p>
+//           <p className="Tags">{post.id}</p>
+//         </div>
+//         <p>{post.body}</p>
+//       </div>
+//     </div>
