51bae12d046140b6a592a9ec87cf46cca5afac6b
Varun <tatireddyvarun20@gmail.com>
2022-11-14 12:32:54 +0000

    Update inputs.jsx

--- src/pages/Login/inputs.jsx
+++ src/pages/Login/inputs.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-function Inputs(){
-    return (
+function Inputs() {
+  return (
+    <div className="row">
+      <form action="">
         <div className="row">
-                <form action="">
-                <div className="row">
-                    <input type="text" id="email" className="email" placeholder="Email"/>
-                </div>
-                <div className="row">
-                    <input type="password" id="password" className="password" placeholder="Password"/>
-                    
-                </div>
-                
-                </form>           
-            
+          <input type="text" id="email" className="email" placeholder="Email" />
         </div>
-    )
+        <div className="row">
+          <input
+            type="password"
+            id="password"
+            className="password"
+            placeholder="Password"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Inputs
+export default Inputs;
