aaf9f8afafbf551a2e7060f265af1ee2d4c96636
varunsai20 <tatireddyvarun20@gmail.com>
2022-11-16 16:41:22 +0530

    add note and delete

--- src/pages/Saved/Post.js
+++ src/pages/Saved/Post.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Saved-Content.css";
+import {Link} from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as MdIcons from "react-icons/md"
 const Post = ({ post }) => {
   return (
     <div className="saved">
       <div className="saved-note">
+      <div className="note-header">
         <a href="">{post.title}</a>
-        <div className="TagFrame">
+        <div className="add-delete">
+        <a href="" className="add-delete-saved" id="add-note-saved"><FaIcons.FaRegStickyNote /><span className="add-note-span">Add a note</span></a>
+        <a href="" className="add-delete-saved" id="delete-note"><MdIcons.MdDeleteForever/><span className="delete-note-span"/>Remove</a>
+        </div>
+        </div>
+      <div className="TagFrame-Saved">
           <p>Tags : </p>
           <p className="Tags">{post.id}</p>
-        </div>
+      </div>
         <p>{post.body}</p>
       </div>
     </div>
