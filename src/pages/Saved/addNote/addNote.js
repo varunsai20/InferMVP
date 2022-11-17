4645b8c20d1548cb58833457f9e723510496621b
varunsai20 <tatireddyvarun20@gmail.com>
2022-11-17 20:09:10 +0530

    Blur filter implementation for addNOte popup

--- src/pages/Saved/addNote/addNote.js
+++ src/pages/Saved/addNote/addNote.js
@@ -10,7 +10,8 @@ export default function AddNote({open,onClose  }) {
   if (!open) return null;
   
   return (
-    <div className="body">
+    <>
+    <div className="body"> </div>
        <div className="add-note-popup">
       <h3 className="header-title">Add a Note</h3>
       <h3 className="note-title">Note title</h3>
@@ -39,6 +40,6 @@ export default function AddNote({open,onClose  }) {
         <button className="cancel-button" onClick={onClose}>Cancel</button>
       </div>
     </div>
-    </div>
+   </>
   );
 }
