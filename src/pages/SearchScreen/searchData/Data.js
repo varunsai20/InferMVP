d4615f68ed99057861f7dc6ba56f584022f91912
varunsai20 <tatireddyvarun20@gmail.com>
2022-11-18 17:49:47 +0530

    Updates

--- src/pages/SearchScreen/searchData/Data.js
+++ src/pages/SearchScreen/searchData/Data.js
@@ -4,9 +4,12 @@ const Data = ({ data ,searchTerm}) => {
   return (
     <div className="SearcContent">
       <div className="note">
-        <a href="{data.htmlFormattedUrl}">{data.title}</a>
+        <div className="search-title-header">
+          <a href="{data.htmlFormattedUrl}">{data.title}</a>
+          <a href="" id="save-tag">SavePost</a>
+        </div>
         <div className="TagFrame">
-          <p>Tags : </p>
+          <p id="Tags">Tags : </p>
           <p className="Tags">{searchTerm}</p>
         </div>
         <p>{data.snippet}</p>
@@ -17,13 +20,3 @@ const Data = ({ data ,searchTerm}) => {
 
 export default Data;
 
-//  <div className="saved">
-//       <div className="note">
-//         <a href="">{post.title}</a>
-//         <div className="TagFrame">
-//           <p>Tags : </p>
-//           <p className="Tags">{post.id}</p>
-//         </div>
-//         <p>{post.body}</p>
-//       </div>
-//     </div>
--- src/pages/SearchScreen/searchData/searchData.js
+++ src/pages/SearchScreen/searchData/searchData.js
@@ -3,11 +3,6 @@ import Data from "./Data";
 const SearchData = ({ googleData,searchTerm }) => {
   return (
     <div>
-      {/* <p className="small text-dark">
-        All
-        {googleData?.searchInformation.formattedTotalResults} these results
-        found in {googleData?.searchInformation.formattedSearchTime}
-      </p> */}
       {googleData?.items.map((data,id) => (
         <Data data={data} key={id} searchTerm={searchTerm}/>
       ))}
@@ -16,13 +11,4 @@ const SearchData = ({ googleData,searchTerm }) => {
 };
 export default SearchData;
 
-{
-  /* <div className="note">
-        <a href="">{post.title}</a>
-        <div className="TagFrame">
-          <p>Tags : </p>
-          <p className="Tags">{post.id}</p>
-        </div>
-        <p>{post.body}</p>
-      </div> */
-}
+
