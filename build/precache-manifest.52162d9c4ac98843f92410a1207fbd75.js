3d821319646c0b49a13f45e7c25b533fa852b94c
varunsai20 <tatireddyvarun20@gmail.com>
2022-11-24 16:42:03 +0530

    build

---
+++ build/precache-manifest.52162d9c4ac98843f92410a1207fbd75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26583f2fc8ec6bcb6e0c5c849499784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1302ebc54d93b1e4e6a5",
+    "url": "/static/css/main.34aa298b.chunk.css"
+  },
+  {
+    "revision": "c515fe284bb2fd42d200",
+    "url": "/static/js/2.15f1a14e.chunk.js"
+  },
+  {
+    "revision": "230ca076ecdfc919f72fe918d8fcbbda",
+    "url": "/static/js/2.15f1a14e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1302ebc54d93b1e4e6a5",
+    "url": "/static/js/main.6d317bcf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6d317bcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db27b536f2868ecf1bd",
+    "url": "/static/js/runtime-main.bcc37542.js"
+  },
+  {
+    "revision": "a17b87e95a1799544568edccca6dc2f7",
+    "url": "/static/media/login-background.a17b87e9.png"
+  }
+]);
